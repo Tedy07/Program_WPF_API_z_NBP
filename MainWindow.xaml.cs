3e6d663accfa3d0a186019c8eed6c587c98a8f3a
Ted K <ted.k07@gmail.com>
2022-10-31 20:25:36 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Program_WPF_API_z_NBP
+{
+   
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static async Task Main()
+        {
+            var httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("http://api.nbp.pl/api/")
+            };
+            var response = await httpClient.GetAsync("exchangerates/rates/A/USD");
+            var content = await response.Content.ReadAsStringAsync();
+            var rate = JsonConvert.DeserializeObject<TableRow>(content);
+            Console.Write(rate.Rates[0].Mid);
+        }
+
+        public class TableRow
+        {
+            public List<Rate> Rates { get; set; }
+        }
+
+        public class Rate
+        {
+            public decimal Mid { get; set; }
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
